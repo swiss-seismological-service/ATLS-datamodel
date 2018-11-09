474f69f84831a3293eb825933236c3f859d48fce
Daniel Armbruster <daniel.armbruster@sed.ethz.ch>
2018-11-09 09:25:05 +0000

    [WIP]: Updated marshmallow version
    Update to marshmallow==3.0.0b13 in order to avoid breaking dependencies
    with ramsis.worker. Note, that this was simply a version update i.e. the
    implementation of ramsis.datamodel is not adapted yet.

--- setup.py
+++ setup.py
@@ -35,7 +35,12 @@ _authors_email = [
     'daniel.armbruster@sed.ethz.ch']
 
 _install_requires= [
-    "marshmallow==2.10.3",
+    # TODO(damb): marshmallow version update due to dependency conflicts.
+    # However, the implementation is not updated yet such that it is quite
+    # probable that the package ramsis.datamodel will break.
+
+    #"marshmallow==2.10.3",
+    "marshmallow==3.0.0b13",
     # TODO (damb): check if valid version: "numpy==1.8.2",
     "numpy>=1.8.2",
     "PyQt5 >=5.8.2, <=5.10",
