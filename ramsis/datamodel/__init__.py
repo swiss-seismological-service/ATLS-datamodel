02728ce3f5769b001e463b194350300838fc747a
Daniel Armbruster <daniel.armbruster@sed.ethz.ch>
2018-12-20 09:15:01 +0000

    [MAINT]: Package documentation __init__.py

--- ramsis/datamodel/__init__.py
+++ ramsis/datamodel/__init__.py
@@ -1,11 +1,6 @@
-# -*- encoding: utf-8 -*-
-# Copyright (C) 2015, SED (ETH Zurich) and Geo-Energie Suisse AG
-
 """
 RT-RAMSIS Data Model
 
-This package defines the classes that store data together with the definitions
-that are required to persist data through the `sqlalchemy` object relational
-mapper.
-
+Package implementing the RT-RAMSIS data model by means of `SQLAlchemy
+<https://www.sqlalchemy.org/>`_ ORM facilities.
 """
