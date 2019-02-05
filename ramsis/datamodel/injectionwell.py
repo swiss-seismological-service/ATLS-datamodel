5596ac72318d7572dcdbb18fd3ddb1ca39237be6
Daniel Armbruster <daniel.armbruster@sed.ethz.ch>
2019-02-05 15:00:03 +0000

    [MAINT]: Refactor injection well facilities

--- ramsis/datamodel/injectionwell.py
+++ ramsis/datamodel/injectionwell.py
@@ -1,58 +1,66 @@
-# -*- encoding: utf-8 -*-
 # Copyright (C) 2013, ETH Zurich - Swiss Seismological Service SED
-
 """
-Injection well information
-
+Injection well ORM facilities.
 """
-
 
 from sqlalchemy import Column, Integer, Boolean, ForeignKey
 from sqlalchemy.orm import relationship
 
-from ramsis.datamodel.base import ORMBase
+from ramsis.datamodel.base import (ORMBase, CreationInfoMixin,
+                                   RealQuantityMixin)
 
 
-class InjectionWell(ORMBase):
+class InjectionWell(CreationInfoMixin,
+                    RealQuantityMixin('welltipx'),
+                    RealQuantityMixin('welltipy'),
+                    RealQuantityMixin('welltipz'),
+                    ORMBase):
     """
-    Injection well information
+    ORM injection well representation (draft state).
 
-    :ivar float well_tip_x: Well tip x coordinate [m]
-    :ivar float well_tip_y: Well tip y coordinate [m]
-    :ivar float well_tip_z: Well tip depth [m] (positive downwards)
+    .. note::
 
+        Dummy implementation. The mapping is currently still in draft state. In
+        future, a well should be defined according to the *real world* needs.
+
+    .. note::
+
+        *Point quantities* are implemented as `QuakeML
+        <https://quake.ethz.ch/quakeml>`_ real quantities.
     """
-    # Project relation
-    project_id = Column(Integer, ForeignKey('projects.id'))
-    project = relationship('Project', back_populates='injection_well')
-    # WellSection relation
-    sections = relationship('WellSection', back_populates='injection_well',
+    # relation: Project
+    project_id = Column(Integer, ForeignKey('project.id'))
+    project = relationship('Project', back_populates='injectionwell')
+
+    # relation: Hydraulics
+    hydraulics = relationship('Hydraulics', back_populates='injectionwell',
+                              cascade='all, delete-orphan')
+    # relation: InjectionPlan
+    injectionplans = relationship('InjectionPlan',
+                                  back_populates='injectionwell',
+                                  cascade='all, delete-orphan')
+
+    # TODO(damb): WellSection is still a dummy implementation.
+    # relation: WellSection
+    sections = relationship('WellSection', back_populates='injectionwell',
                             cascade='all, delete-orphan')
-    # endregion
-
-    # Data attributes (required for flattening)
-    data_attrs = ['well_tip_z', 'well_tip_x', 'well_tip_y']
-
-    def __init__(self, well_tip_z, well_tip_x, well_tip_y):
-        """
-        The initialisation parameters are the same as the member variables.
-        See class description for details.
-
-        """
-        self.well_tip_x = well_tip_x
-        self.well_tip_y = well_tip_y
-        self.well_tip_z = well_tip_z
 
     @property
     def injection_point(self):
-        # TODO: implement
+        # TODO(damb): To be implemented.
         return 4740.3, 270645.0, 611631.0
+
+# class InjectionWell
 
 
 class WellSection(ORMBase):
+    """
+    Dummy ORM implementation of a well section.
+    """
+    # TODO(damb): E.g. add positional information
     cased = Column(Boolean)
-    # TODO: add position
-    # InjectionWell relation
-    injection_well_id = Column(Integer, ForeignKey('injection_wells.id'))
-    injection_well = relationship('InjectionWell', back_populates='sections')
-    # endregion
+    # relation: InjectionWell
+    injectionwell_id = Column(Integer, ForeignKey('injectionwell.id'))
+    injectionwell = relationship('InjectionWell', back_populates='sections')
+
+# class WellSection
