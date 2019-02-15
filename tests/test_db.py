242b724d6fa42c6e34e9a37e3e984a6c5b02cc97
Daniel Armbruster <daniel.armbruster@sed.ethz.ch>
2019-02-15 13:21:32 +0000

    [STY]: PEP8

--- tests/test_db.py
+++ tests/test_db.py
@@ -10,20 +10,21 @@ from sqlalchemy import create_engine
 from sqlalchemy.event import listen
 from sqlalchemy.sql import select, func
 
-from ramsis.datamodel.status import Status
-from ramsis.datamodel.seismicity import SeismicityModel
-from ramsis.datamodel.forecast import Forecast
-from ramsis.datamodel.seismics import SeismicCatalog
-from ramsis.datamodel.well import InjectionWell
-from ramsis.datamodel.settings import ProjectSettings
-from ramsis.datamodel.project import Project
-from ramsis.datamodel.base  import ORMBase
+from ramsis.datamodel.status import Status # noqa
+from ramsis.datamodel.seismicity import SeismicityModel # noqa
+from ramsis.datamodel.forecast import Forecast # noqa
+from ramsis.datamodel.seismics import SeismicCatalog # noqa
+from ramsis.datamodel.well import InjectionWell # noqa
+from ramsis.datamodel.settings import ProjectSettings # noqa
+from ramsis.datamodel.project import Project # noqa
+from ramsis.datamodel.base import ORMBase
+
 
 def load_spatialite(dbapi_conn, connection_record):
     """
     Load spatialite extension.
     """
-    #XXX(damb): sudo apt-get install libsqlite3-mod-spatialite
+    # XXX(damb): sudo apt-get install libsqlite3-mod-spatialite
     dbapi_conn.enable_load_extension(True)
     dbapi_conn.load_extension('/usr/lib/x86_64-linux-gnu/mod_spatialite.so')
 
@@ -34,7 +35,7 @@ class GISDBTestCase(unittest.TestCase):
 
     def setUp(self):
         _, self.path_db = tempfile.mkstemp(dir=tempfile.gettempdir())
-        #XXX(damb): see:
+        # XXX(damb): see:
         # https://geoalchemy-2.readthedocs.io/en/latest/spatialite_tutorial.html
         self.engine = create_engine('sqlite:///{}'.format(self.path_db))
         listen(self.engine, 'connect', load_spatialite)
@@ -52,4 +53,3 @@ class GISDBTestCase(unittest.TestCase):
         self.assertIsNone(ORMBase.metadata.create_all(self.engine))
 
 # class GISDBTestCase
-
