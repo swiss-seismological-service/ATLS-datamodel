7d4caba2cbbacc991922be0166575b1466ff1162
Daniel Armbruster <daniel.armbruster@sed.ethz.ch>
2019-07-19 11:05:45 +0000

    [TST] Merge with forced time window

--- tests/test_seismics.py
+++ tests/test_seismics.py
@@ -154,7 +154,8 @@ class SeismicCatalogTestCase(unittest.TestCase):
 
         c2 = SeismicCatalog(
             events=[SeismicEvent(
-                datetime_value=c2_first_event + i * c2_interval, magnitude=i)
+                datetime_value=c2_first_event + i * c2_interval,
+                magnitude_value=i)
                 for i in range(c2_num_events)])
 
         c1.merge(c2)
@@ -162,6 +163,138 @@ class SeismicCatalogTestCase(unittest.TestCase):
         self.assertEqual(len(c1), len(c2))
         for e_c1, e_c2 in zip(c1, c2):
             self.assertEqual(e_c1, e_c2)
+
+    def test_merge_with_starttime(self):
+        c1_first_event = datetime.datetime(2020, 1, 1)
+        c1_interval = datetime.timedelta(seconds=3600)
+        c1_num_events = 7
+
+        c1 = SeismicCatalog(
+            events=[SeismicEvent(
+                datetime_value=c1_first_event + i * c1_interval)
+                for i in range(c1_num_events)])
+
+        c2_first_event = datetime.datetime(2020, 1, 1, 3)
+        c2_interval = datetime.timedelta(seconds=1800)
+        c2_num_events = 4
+
+        c2 = SeismicCatalog(
+            events=[SeismicEvent(
+                datetime_value=c2_first_event + i * c2_interval)
+                for i in range(c2_num_events)])
+
+        c1.merge(c2, starttime=datetime.datetime(2020, 1, 1, 2))
+
+        self.assertEqual(
+            SeismicEvent(datetime_value=datetime.datetime(2020, 1, 1)),
+            c1.events[0])
+        self.assertEqual(
+            SeismicEvent(datetime_value=datetime.datetime(2020, 1, 1, 1)),
+            c1.events[1])
+        self.assertEqual(
+            SeismicEvent(datetime_value=datetime.datetime(2020, 1, 1, 5)),
+            c1.events[2])
+        self.assertEqual(
+            SeismicEvent(datetime_value=datetime.datetime(2020, 1, 1, 6)),
+            c1.events[3])
+        self.assertEqual(
+            SeismicEvent(datetime_value=datetime.datetime(2020, 1, 1, 3)),
+            c1.events[4])
+        self.assertEqual(
+            SeismicEvent(datetime_value=datetime.datetime(2020, 1, 1, 3, 30)),
+            c1.events[5])
+        self.assertEqual(
+            SeismicEvent(datetime_value=datetime.datetime(2020, 1, 1, 4)),
+            c1.events[6])
+        self.assertEqual(
+            SeismicEvent(datetime_value=datetime.datetime(2020, 1, 1, 4, 30)),
+            c1.events[7])
+
+    def test_merge_with_endtime(self):
+        c1_first_event = datetime.datetime(2020, 1, 1)
+        c1_interval = datetime.timedelta(seconds=3600)
+        c1_num_events = 7
+
+        c1 = SeismicCatalog(
+            events=[SeismicEvent(
+                datetime_value=c1_first_event + i * c1_interval)
+                for i in range(c1_num_events)])
+
+        c2_first_event = datetime.datetime(2020, 1, 1, 3)
+        c2_interval = datetime.timedelta(seconds=1800)
+        c2_num_events = 4
+
+        c2 = SeismicCatalog(
+            events=[SeismicEvent(
+                datetime_value=c2_first_event + i * c2_interval)
+                for i in range(c2_num_events)])
+
+        c1.merge(c2, endtime=datetime.datetime(2020, 1, 1, 4))
+
+        self.assertEqual(
+            SeismicEvent(datetime_value=datetime.datetime(2020, 1, 1)),
+            c1.events[0])
+        self.assertEqual(
+            SeismicEvent(datetime_value=datetime.datetime(2020, 1, 1, 1)),
+            c1.events[1])
+        self.assertEqual(
+            SeismicEvent(datetime_value=datetime.datetime(2020, 1, 1, 2)),
+            c1.events[2])
+        self.assertEqual(
+            SeismicEvent(datetime_value=datetime.datetime(2020, 1, 1, 5)),
+            c1.events[3])
+        self.assertEqual(
+            SeismicEvent(datetime_value=datetime.datetime(2020, 1, 1, 6)),
+            c1.events[4])
+        self.assertEqual(
+            SeismicEvent(datetime_value=datetime.datetime(2020, 1, 1, 3)),
+            c1.events[5])
+        self.assertEqual(
+            SeismicEvent(datetime_value=datetime.datetime(2020, 1, 1, 3, 30)),
+            c1.events[6])
+        self.assertEqual(
+            SeismicEvent(datetime_value=datetime.datetime(2020, 1, 1, 4)),
+            c1.events[7])
+
+    def test_merge_with_timewindow_no_events(self):
+        c1_first_event = datetime.datetime(2020, 1, 1)
+        c1_interval = datetime.timedelta(seconds=3600)
+        c1_num_events = 7
+
+        c1 = SeismicCatalog(
+            events=[SeismicEvent(
+                datetime_value=c1_first_event + i * c1_interval)
+                for i in range(c1_num_events)])
+
+        c2_first_event = datetime.datetime(2020, 1, 1, 3)
+        c2_interval = datetime.timedelta(seconds=1800)
+        c2_num_events = 4
+
+        c2 = SeismicCatalog(
+            events=[SeismicEvent(
+                datetime_value=c2_first_event + i * c2_interval)
+                for i in range(c2_num_events)])
+
+        # no events in c2 available from starttime until endtime - though, c1
+        # is reduced
+        c1.merge(c2, starttime=datetime.datetime(2020, 1, 1, 1),
+                 endtime=datetime.datetime(2020, 1, 1, 2))
+
+        self.assertEqual(
+            SeismicEvent(datetime_value=datetime.datetime(2020, 1, 1)),
+            c1.events[0])
+        self.assertEqual(
+            SeismicEvent(datetime_value=datetime.datetime(2020, 1, 1, 3)),
+            c1.events[1])
+        self.assertEqual(
+            SeismicEvent(datetime_value=datetime.datetime(2020, 1, 1, 4)),
+            c1.events[2])
+        self.assertEqual(
+            SeismicEvent(datetime_value=datetime.datetime(2020, 1, 1, 5)),
+            c1.events[3])
+        self.assertEqual(
+            SeismicEvent(datetime_value=datetime.datetime(2020, 1, 1, 6)),
+            c1.events[4])
 
 
 def suite():
