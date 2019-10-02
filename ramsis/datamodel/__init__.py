bb4a1130d59eb1fcb66d3b7547ed242be31939b9
Daniel Armbruster <daniel.armbruster@sed.ethz.ch>
2019-10-02 14:09:27 +0000

    [ENH] Hide internal structure
    Expose an API for ramsis.datamodel.

--- ramsis/datamodel/__init__.py
+++ ramsis/datamodel/__init__.py
@@ -6,3 +6,23 @@ Package implementing the RT-RAMSIS data model by means of `SQLAlchemy
 <https://www.sqlalchemy.org/>`_ ORM facilities. Spatial facilities are provided
 by `GeoAlchemy2 <https://geoalchemy-2.readthedocs.io/en/latest/>`_.
 """
+
+from ramsis.datamodel.base import ORMBase  # noqa
+from ramsis.datamodel.forecast import (  # noqa
+    Forecast, ForecastScenario, ForecastStage, SeismicityForecastStage,
+    SeismicitySkillStage, HazardStage, RiskStage, EStage)
+from ramsis.datamodel.hydraulics import (  # noqa
+    Hydraulics, InjectionPlan, HydraulicSample)
+from ramsis.datamodel.model import (  # noqa
+    EModel, Model, ModelRun)
+from ramsis.datamodel.project import Project  # noqa
+from ramsis.datamodel.seismicity import (  # noqa
+    SeismicityModel, SeismicityModelRun, ReservoirSeismicityPrediction,
+    SeismicityPredictionBin)
+from ramsis.datamodel.seismics import (  # noqa
+    SeismicCatalog, SeismicEvent)
+from ramsis.datamodel.settings import (  # noqa
+    Settings, ProjectSettings)
+from ramsis.datamodel.status import Status, EStatus  # noqa
+from ramsis.datamodel.well import (  # noqa
+    InjectionWell, WellSection)
